e84322e087313db625bae6e0ccc942b13c6660aa
Chandra Shekhar <pandeynihal674@gmail.com>
2026-07-10 12:14:43 +0530

    add new code file

---
+++ DependencyInjection.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI , Depends, Header, HTTPException
+app = FastAPI()
+
+def varify_token(token:str = Header(None)):
+    if token != "mysecrettoken":
+        raise HTTPException(
+            status_code=401,
+            detail="unauthorized"
+        )
+    return{
+        "user":"Authorized User"
+
+    }
+
+@app.get("/secure-data")
+def secure_data(user = Depends(varify_token)):
+    return {
+        "message":"secure data accessed",
+        "user":user
+
+    }
+
+# def common_logic():
+#     return{
+#         "message":"Common Logic executed"
+#     }
+
+# @app.get("/home")
+# def home(data = Depends(common_logic)):
+#     return data
+
+# def get_current_user():
+#     return{
+#         "user":"Mohit"
+#     }
+
+# @app.get("/profile")
+# def profile(user = Depends(get_current_user)):
+#     return user
+
+# @app.get("/dashboard")
+# def dashboard(user = Depends(get_current_user)):
+#     return user
